64f04f8eb679f526a1a54589b6078adba27049d0
francisco goncalves <kiko.cbg@gmail.com>
2020-03-17 11:41:31 +0100

    Added: Reseter and ReseterTest;

--- src/Reseter.php
+++ src/Reseter.php
@@ -1,3 +1,19 @@
 <?php
 namespace App;
 
+final class Reseter
+{
+
+    public function peopleAliveAgain($deadList)
+    {
+
+        for ($index = 0; $index < count($deadList); $index++) {
+            $deadList[$index]['status'] = 'Alive';
+        }
+
+        $alivePeople = $deadList;
+
+
+        return $alivePeople;
+    }
+}
--- tests/ReseterTest.php
+++ tests/ReseterTest.php
@@ -2,3 +2,29 @@
 use PHPUnit\Framework\TestCase;
 use App\Reseter;
 
+final class ReseterTest extends TestCase
+{
+
+    private $deadList = array();
+
+    public function setUp(): void {
+
+        $p1 = Array('id' => 1, 'name' => 'Jimmy', 'status' => 'Dead');
+        $p2 = Array('id' => 2, 'name' => 'Bob', 'status' => 'Dead');
+        $p3 = Array('id' => 3, 'name' => 'Jim', 'status' => 'Dead');
+
+        array_push($this->deadList, $p1, $p2, $p3);
+    }
+
+    public function testIfPeopleAreAliveAgain()
+    {
+        $reseter = new Reseter();
+
+        $aliveList = $reseter->peopleAliveAgain($this->deadList);
+
+        $this->assertEquals('Alive', $aliveList[0]['status']);
+        $this->assertEquals('Alive', $aliveList[1]['status']);
+        $this->assertEquals('Alive', $aliveList[2]['status']);
+    }
+
+}
